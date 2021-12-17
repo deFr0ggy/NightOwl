778153a4e34b33f68014f3bed6d1113861252236
Frog Man <16836050+deFr0ggy@users.noreply.github.com>
2021-12-17 22:18:32 +0500

    Add files via upload

---
+++ NightOwl.py
@@ -0,0 +1,215 @@
+import json
+import sys
+import os
+import re
+import colorama
+import requests
+import socket
+from colorama import Fore, Style
+from requests.models import HTTPBasicAuth
+colorama.init(autoreset=True)
+
+global count
+
+AbusedIPDB_API = ""
+
+def baseGrabber():
+
+    print(Fore.BLUE + "-"*50)
+    print(Fore.BLUE + "Printing Details You Should Care About!")
+    print(Fore.BLUE + "-"*50 + "\n")
+    count = 0
+    with open(sys.argv[1], "r", encoding="utf-8") as sample:
+        for line in sample:
+            if line.startswith("From: "):
+                print(Fore.RED + line)
+            if line.startswith("To: "):
+                print(Fore.YELLOW + line)   
+            if line.startswith("Subject: "):
+                print(Fore.GREEN + line)
+            if line.startswith("Date: "):
+                print(Fore.RED + line) 
+            if line.startswith("Message Body: "):
+                print(Fore.GREEN + line)
+            if line.startswith("Received: "):
+                count += 1
+            if line.startswith("Message-ID: "):
+                print(Fore.GREEN + line)
+
+
+    print("-> Total HOPS Count: " + str(count) + "\n")
+
+
+def ipGrabber():
+    print(Fore.BLUE + "-"*50)
+    print(Fore.BLUE + "Printing The Unique IP Addresses Only!")
+    print(Fore.BLUE + "-"*50)
+    fileOpen = open(sys.argv[1],'r', encoding='utf-8')
+    readText = fileOpen.read()
+    IP = [] 
+    IP_COUNT = 0
+    regex = re.findall(r'\b(?:\d{1,3}\.){3}\d{1,3}\b',readText)
+    if regex is not None:
+        for match in regex:
+            if match not in IP:
+                IP.append(match)
+                IP_COUNT += 1
+                print("\n" + str(IP_COUNT) + Fore.YELLOW + " - IP Address: " + match)
+                # abusedIP(match)
+                # print("\n")
+
+def emailGrabber():
+    print(Fore.BLUE + "-"*50)
+    print(Fore.BLUE + "Butchering Emails!")
+    print(Fore.BLUE + "-"*50)
+    fileOpen = open(sys.argv[1],'r', encoding='utf-8')
+    readText = fileOpen.read()
+    EMAIL = [] 
+    regex = re.findall(r'[\w\.-]+@[\w\.-]+', readText)
+    if regex is not None:
+        for match in regex:
+            if match not in EMAIL:
+                EMAIL.append(match)
+                print(Fore.YELLOW + match + "\n")
+
+def urlGrabber():
+    print(Fore.BLUE + "-"*50)
+    print(Fore.BLUE + "Butchering All The URLs!")
+    print(Fore.BLUE + "-"*50 + "\n")
+    fileOpen = open(sys.argv[1],'r', encoding='utf-8')
+    readText = fileOpen.read()
+    print(re.search("(?P<url>https?://[^\s]+)", readText).group("url"))  
+    URL = [] 
+
+    regex = re.findall(r'(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.)+[a-z0-9][a-z0-9-]{0,61}[a-z0-9]',readText)
+    if regex is not None:
+        for match in regex:
+            if match not in URL:
+                URL.append(match)
+                
+
+    if not URL:
+        print(Fore.GREEN + "There were no URLs Found!")
+    else:
+        print("\n" + Fore.YELLOW + match)
+        IP_ADDR = socket.gethostbyname(match)
+        print("\n" + "Domain: " + match + "\n" + "IP Address: "+ IP_ADDR)
+
+        # abusedIP(IP_ADDR)
+        # whoisit(match)
+
+def htmlHunter():
+    print("\n")
+    print(Fore.BLUE + "-"*50)
+    print(Fore.BLUE + "Checking If There Is An HTML File Embedded")
+    print(Fore.BLUE + "-"*50)
+    contents = []
+    with open(sys.argv[1],'r', encoding='utf-8') as sample:
+        for line in sample:
+            if line.startswith("<!DOCTYPE") or line.startswith("<html>"):
+                contents.append(line)
+                for line in sample:
+                    contents.append(line)
+    if not contents:
+        print(Fore.GREEN + "There was no HTML file embedded!")
+    else:
+        print(Fore.BLUE + "\n" + "-"*50)
+        conscent = input("HTML Contents Found - Do you want to save it? [Y/N]: ")
+        print(Fore.BLUE + "-"*50)
+        if conscent == "y" or conscent == "Y":
+            outfile = input("Provide File Name: ")
+            htmlData = open(outfile, "w")
+            htmlData.write("".join(contents).strip("\n"))
+            htmlData.close
+            print("\n")
+            print(Fore.BLUE + "-"*50)
+            print(Fore.GREEN + "The HTML File is extracted: " + outfile)
+            print(Fore.BLUE + "-"*50)
+        else:
+            exit
+
+
+def xHunter():
+    print("\n")
+    print(Fore.BLUE + "-"*50)
+    print(Fore.BLUE + "Printing All The Headers Which Were Added During The Email Travel")
+    print(Fore.BLUE + "-"*50 + "\n")
+
+    with open(sys.argv[1],'r', encoding='utf-8') as sample:
+        for line in sample:
+            if line.startswith("X-"):
+                print(Fore.YELLOW + line)
+    print(Fore.BLUE + "-"*50 + "\n")
+
+def abusedIP(ip):
+
+    print("\n" + Fore.MAGENTA + "ABUSEDIPDB Response!" + "\n")
+
+    url = 'https://api.abuseipdb.com/api/v2/check'
+
+    querystring = {
+        'ipAddress': ip,
+        'maxAgeInDays': '365'
+    }
+
+    headers = {
+        'Accept': 'application/json',
+        'Key': AbusedIPDB_API
+    }
+
+    response = requests.request(method='GET', url=url, headers=headers, params=querystring)
+    decodedResponse = json.loads(response.text)
+    print(json.dumps(decodedResponse, sort_keys=True, indent=4))
+
+def whoisit(domain):
+    print("\n"+ Fore.BLUE +"*"*50)
+    print("WHOIS Information - " + domain)
+    print("\n"+ Fore.BLUE +"*"*50)
+    os.system('whois '+domain)
+    print("\n"+ Fore.BLUE +"*"*50+"\n")
+   
+def banner():
+
+    banner = """
+    
+██████   █████  ███           █████       █████          ███████                    ████ 
+░░██████ ░░███  ░░░           ░░███       ░░███         ███░░░░░███                 ░░███ 
+ ░███░███ ░███  ████   ███████ ░███████   ███████      ███     ░░███ █████ ███ █████ ░███ 
+ ░███░░███░███ ░░███  ███░░███ ░███░░███ ░░░███░      ░███      ░███░░███ ░███░░███  ░███ 
+ ░███ ░░██████  ░███ ░███ ░███ ░███ ░███   ░███       ░███      ░███ ░███ ░███ ░███  ░███ 
+ ░███  ░░█████  ░███ ░███ ░███ ░███ ░███   ░███ ███   ░░███     ███  ░░███████████   ░███ 
+ █████  ░░█████ █████░░███████ ████ █████  ░░█████     ░░░███████░    ░░████░████    █████
+░░░░░    ░░░░░ ░░░░░  ░░░░░███░░░░ ░░░░░    ░░░░░        ░░░░░░░       ░░░░ ░░░░    ░░░░░ 
+                      ███ ░███                                                            
+                     ░░██████                                                             
+                      ░░░░░░                                                              
+
+
+    OFFLINE PHISHING EMAIL BUTCHER
+    Coded by Kamran Saifullah - Frog Man
+    -----------------------------------------
+    Usage: ./NightOwl.py <Mices>
+    -----------------------------------------
+    LinkedIn: https://www.linkedin.com/in/kamransaifullah/
+    GitHub: https://github.com/deFr0ggy
+    Twitter: https://twitter.com/deFr0ggy
+    """
+
+    print(Fore.GREEN + banner + "\n")
+
+def main():
+
+    banner()
+
+    if len(sys.argv) < 2 or len(sys.argv) > 2:
+        print(Fore.YELLOW + "Invalid number of arguments provided!")
+    else:
+        baseGrabber()
+        emailGrabber()
+        ipGrabber()
+        urlGrabber()
+        xHunter()
+        htmlHunter()
+
+if __name__ == "__main__":
+    main()
---
+++ README.md
@@ -0,0 +1,91 @@
+<h1 align="center">
+  <br>
+  <a href="https://github.com/deFr0ggy/NightOwl/"><img src="https://i.imgur.com/14nkmdR.png" alt="Email Butcher"></a>
+</h1>
+
+<h4 align="center">Python script to butcher Email Headers</h4>
+
+Every organization receives SPAM, Phishing and Spear Phishing Emails. Whenever such emails are received the first and the foremost actions required is to get the Email Headers which are then shared either with the MSSP currently looking after your security or with the internal Security Analyst. The reason is to find all the required information i.e. to perform Email Forensics in order to find answers like. 
+
+- Who sent the email?
+- From where it originated?
+- How many people have received the email?
+- Does the email contained a phishing link or it contained only plain text?
+- Does the email contains any attachments?
+
+These are few of the questions which always come to our mind when any such email is received. Keeping these details in mind I have developed **Email Butcher** to automate the manual procedure to find all possible answers to our questions. 
+
+---
+
+**Email Butcher** is a quick and dirty python script to perform analysis on Email Headers. The script renders all the required information for you to take quick actions rather than going through the headers line by line. **The script does it for you** automatically.
+
+I have developed this script during my day job and I do plan to take it one step further with time. The requirements for the program are as below.
+
+```
+1. Python3
+2. Colorama
+```
+## Usage
+```
+     _____ __  __    _    ___ _       ____  _   _ _____ ____ _   _ _____ ____  
+    | ____|  \/  |  / \  |_ _| |     | __ )| | | |_   _/ ___| | | | ____|  _ \ 
+    |  _| | |\/| | / _ \  | || |     |  _ \| | | | | || |   | |_| |  _| | |_) |
+    | |___| |  | |/ ___ \ | || |___  | |_) | |_| | | || |___|  _  | |___|  _ < 
+    |_____|_|  |_/_/   \_\___|_____| |____/ \___/  |_| \____|_| |_|_____|_| \_ 
+
+    Coded by deFr0ggy - An Abnormal Frog!
+    Usage: ./EmailButcher.py <EmailHeadersFile>
+    
+```
+## Installing Dependencies
+
+- pip install -r requirements.txt
+- python -m pip install -r requirements.txt
+
+## Butchering Basic Email Headers
+
+These headers include the following.
+- TO
+- FROM 
+- SUBJECT
+- DATE
+
+## Hops Count
+
+The script counts for the total number of hops (MTAs/MDAs). Thusm we can calculate the total number of MTAs/MDAs involved since the email generation to its landing in our inbox.
+
+## Butchering IP Addresses
+
+The script then looks for all the IP addresses from the Email Headers and lists them down for analysis. These are the IP addresses which are of MTAs/MDAs, Receivers and any other IP addresses embedded within the email or elsewhere. It hunts for all.
+
+## Butchering Email Addresses
+
+The script looks for all the available email addresses from within the Email Headers and lists them down. This is to check which mail servers, senders, receivers are involved. 
+
+## Butchering URLs
+
+The script also looks for all the available URLs from the email headers and provides us with the information to take quick actions i.e. to get these URLs blocked on Email Gateways, Firewalls etc to prevent infections proactively.
+
+## Butchering HTML Embedded Contents
+
+The script hunts down all the HTML contents if it is present in the Email Headers. It alerts that there is an HTML content present in the email and asks for the output file where it can write that data to for later analysis.
+
+## Listing Down All X-* Headers
+
+These headers provide a wide range of information. Also, these headers gets added to the Email as soon as the email starts to propogate. These can provides us with the information whether the email is a SPAM, Malicious, routed through MS-Exchange etc. Close attention is required for these headers.
+
+---
+
+# To Do 
+
+In ***Phase 2***, I need to add the following functionalities along with revamping the overall code from **Quick & Dirty** to **Quick & More Organized**.
+
+- [ ] Integrate Virustotal
+- [ ] Integrate Talos 
+- [X] Integrate AbuseIPDB
+- [ ] Integrate URLVoid
+- [ ] Integrate Phishtank
+- [ ] Perform IP lookups
+- [ ] Gather Domains/IPs country information.
+- [ ] Gather Domains Hosting Information
+- [X] Add Support For UTF-8 Encoding Scheme
